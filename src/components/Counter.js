c7d7353b94e54938a27bb5f8cd542c2e06bb1549
makhitr <ring_ru@yahoo.com>
2023-03-09 00:20:26 +0300

    feat: counter, genreSelect, seahForm

---
+++ src/components/Counter.js
@@ -0,0 +1,23 @@
+import React from "react";
+import '../styles/counter.css'
+
+export default class Counter extends React.Component {
+  state = {
+    count: 0
+  }
+
+  subtract = () => {
+    this.setState(prevState => ({ count: prevState.count - 1 }))
+  }
+  add = () => {
+    this.setState(prevState => ({ count: prevState.count + 1 }))
+  }
+  render() {
+    const subtractBtn = React.createElement("button", { className: `counterBtn minusBtn`, onClick: this.subtract }, "-",)
+    const addBtn = React.createElement("button", { className: "counterBtn plusBtn", onClick: this.add }, "+")
+    const count = React.createElement("div", { className: "counterCount" }, this.state.count)
+    const countEl = React.createElement("div", { className: "counterWrapper" }, subtractBtn, count, addBtn)
+
+    return countEl
+  }
+}
